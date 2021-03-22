84890047564cd09766a9cd53c0f60a5866ccf94f
Anatolii Sakhnik <sakhnik@gmail.com>
2021-03-22 17:32:27 +0200

    Try fixing the path to mingw64

--- scripts/package.sh
+++ scripts/package.sh
@@ -10,11 +10,11 @@ cp nvv.exe dist/bin
 
 dlls=$(ntldd.exe -R nvv.exe | grep -Po "[^ ]+?msys64[^ ]+" | sort -u | grep -Po '[^\\]+$')
 for dll in $dlls; do
-  cp /c/msys64/mingw64/bin/$dll dist/bin
+  cp /mingw64/bin/$dll dist/bin
 done
 
-cp -R /c/msys64/mingw64/lib/gdk-pixbuf-2.0 dist/lib/
-cp -R /c/msys64/mingw64/share/{glib-2.0,icons} dist/share/
+cp -R /mingw64/lib/gdk-pixbuf-2.0 dist/lib/
+cp -R /mingw64/share/{glib-2.0,icons} dist/share/
 
 pushd dist
 zip -r ../nvim-win64.zip *
