a386c41152f62dbe07c7492a09d7c3979ffde145
Anatolii Sakhnik <sakhnik@gmail.com>
2021-03-22 18:11:55 +0200

    Fix package names

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,17 +4,18 @@ if [[ "$WORKSPACE_MSYS2" ]]; then
   cd "$WORKSPACE_MSYS2"
 fi
 
-cd build
-mkdir -p dist/{bin,lib,share}
-cp nvv.exe dist/bin
+mkdir -p dist/nvv/{bin,lib,share}
+cp build/nvv.exe dist/nvv/bin
 
-dlls=$(ntldd.exe -R nvv.exe | grep -Po "[^ ]+?msys64[^ ]+" | sort -u | grep -Po '[^\\]+$')
+dlls=$(ntldd.exe -R build/nvv.exe | grep -Po "[^ ]+?msys64[^ ]+" | sort -u | grep -Po '[^\\]+$')
 for dll in $dlls; do
-  cp /mingw64/bin/$dll dist/bin
+  cp /mingw64/bin/$dll dist/nvv/bin
 done
+cp /mingw64/bin/gspawn* dist/nvv/bin
 
-cp -R /mingw64/lib/gdk-pixbuf-2.0 dist/lib/
-cp -R /mingw64/share/{glib-2.0,icons} dist/share/
+cp -R /mingw64/lib/gdk-pixbuf-2.0 dist/nvv/lib/
+cp -R /mingw64/share/{glib-2.0,icons} dist/nvv/share/
 
 pushd dist
-zip -r ../nvim-win64.zip *
+zip -r nvv-win64.zip *
+popd
